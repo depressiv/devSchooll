9224cc3dcd1ae984e00022daa1f012b7822ebfa9
depressiv <tinhoribeiro.rr@gmail.com>
2021-09-13 14:13:36 -0300

    foi

--- react/site/src/pages/alunos/index.js
+++ react/site/src/pages/alunos/index.js
@@ -4,8 +4,101 @@ import Menu from '../../components/menu'
 
 import { Container, Conteudo } from './styled'
 
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; 
+
+import { useState, useEffect } from 'react'
+
+import Api from '../../service/api'
+const api = new Api();
 
 export default function Index() {
+
+    const [alunos, setAlunos] = useState([ ]);
+    const [nome, setNome] = useState([ ]);
+    const [chamada, setChamada] = useState([ ]);
+    const [turma, setTurma] = useState([ ]);
+    const [curso, setCurso] = useState([ ]);
+    const [idAlterando, setIdAlterando] = useState(0);
+    
+    async function listar(){
+        let r = await api.listar();
+        setAlunos(r);
+    }
+
+    
+
+    async function inserir(){
+        if (idAlterando == 0) {
+            let r = await api.inserir(nome, chamada, curso, turma);
+        
+            if(r.erro) 
+                alert(r.erro);
+            else    
+                alert('aluno inserido!');
+        } else {
+            let r = await api.alterar(idAlterando, nome, chamada, curso, turma)
+            
+        if(r.erro) 
+            alert(r.erro);
+        else 
+            alert('aluno alterado!');
+        }
+        
+        LimparCampos();
+        listar();
+    }
+
+    function LimparCampos(){
+        setNome('');
+        setChamada('');
+        setCurso('');
+        setTurma('');
+        setIdAlterando(0);
+    }
+
+    async function remover(id)  {
+        confirmAlert({
+            title: 'remover aluno',
+            message: `tem certeza que deseja remover o aluno ${id} ?`,
+            buttons: [
+                {
+                    label: 'sim',
+                    onClick: async () => {
+                        let r = await api.remover(id);
+                        if (r.erro)
+                            toast.error(`${r.erro}`);
+                            else{
+                                toast.dark('aluno removido!');
+                                listar();
+                            }
+                        }
+                },
+                {
+                    label: 'não'
+                }
+            ]
+          });
+    }
+    
+    async function editar(item) {
+        setNome(item.nm_aluno);
+        setChamada(item.nr_chamada);
+        setCurso(item.nm_curso);
+        setTurma(item.nm_turma);
+        setIdAlterando(item.id_matricula);
+        
+    }
+
+        useEffect(() => {
+        listar();
+    }, [])
+
+    
+    
     return (
         <Container>
             <Menu />
@@ -16,32 +109,32 @@ export default function Index() {
                         
                         <div class="text-new-student">
                             <div class="bar-new-student"></div>
-                            <div class="text-new-student">Novo Aluno</div>
+                            <div class="text-new-student">{ idAlterando == 0 ? "Novo Aluno" : "Alterando Aluno"  + idAlterando } </div>
                         </div>
 
                         <div class="input-new-student"> 
                             <div class="input-left">
                                 <div class="agp-input"> 
                                     <div class="name-student"> Nome: </div>  
-                                    <div class="input"> <input /> </div>  
+                                    <div class="input"> <input type="text" value={nome} onChange={e => setNome(e.target.value)}/> </div>  
                                 </div> 
                                 <div class="agp-input">
                                     <div class="number-student"> Chamada: </div>  
-                                    <div class="input"> <input /> </div> 
+                                    <div class="input"> <input type="text" value={chamada} onChange={e => setChamada(e.target.value)}/> </div> 
                                 </div>
                             </div>
 
                             <div class="input-right">
                                 <div class="agp-input">
                                     <div class="corse-student"> Curso: </div>  
-                                    <div class="input"> <input /> </div>  
+                                    <div class="input"> <input type="text" value={curso} onChange={e => setCurso(e.target.value)}/> </div>  
                                 </div>
                                 <div class="agp-input">
                                     <div class="class-student"> Turma: </div>  
-                                    <div class="input"> <input /> </div> 
+                                    <div class="input"> <input type="text" value={turma} onChange={e => setTurma(e.target.value)} /> </div> 
                                 </div>
                             </div>
-                            <div class="button-create"> <button> Cadastrar </button> </div>
+                            <div class="button-create"> <button onClick={inserir}> {idAlterando == 0 ? "cadastrar" : "alterar" } </button> </div>
                         </div>
                     </div>
 
@@ -65,46 +158,24 @@ export default function Index() {
                             </thead>
                     
                             <tbody>
-                                <tr>
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 15 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> <button> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
-                                    <td> <button> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
-                                </tr>
-                            
-                                <tr class="linha-alternada">
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 16 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> </td>
-                                    <td> </td>
+
+                                {alunos.map((item, i) =>
+
+                                <tr className={i % 2 == 0 ? "linha-alternada" : "" }>
+                                    <td> {item.id_matricula} </td>
+                                    <td title = {item.nm_aluno}>
+                                    {item.nm_aluno != null && item.nm_aluno.lenght >= 25
+                                         ? item.nm_aluno.substr(0, 25) + "..." 
+                                        : item.nm_aluno } </td>
+                                    <td> {item.nr_chamada} </td>
+                                    <td> {item.nm_turma} </td>
+                                    <td> {item.nm_curso} </td>
+                                    <td className="coluna-acao"> <button onClick={() => editar(item)}> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
+                                    <td className="coluna-acao"> <button onClick={() => remover(item.id_matricula)}> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
                                 </tr>
 
-                                <tr>
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 17 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> </td>
-                                    <td> </td>
-                                </tr>
-
-                                <tr class="linha-alternada">
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 18 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> </td>
-                                    <td> </td>
-                                </tr>
-                                
+                            )}
+                                     
                             </tbody> 
                         </table>
                     </div>
